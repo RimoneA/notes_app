5c056ddf8308946005cc5966397ae9e787134dde
Rimone <rimoneazmy1@gmail.com>
2025-03-30 00:29:12 +0200

    Create edit note view

---
+++ lib/widgets/edit_note_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'edit_note_body.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: EditNoteViewBody());
+  }
+}
