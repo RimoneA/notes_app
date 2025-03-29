a15461995dff1f4f611f33067a7f7ff5beba757b
Rimone <rimoneazmy1@gmail.com>
2025-03-30 00:11:00 +0200

    finish notes bottom sheet

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.hint, this.maxLine = 1});
 
+  final String hint;
+  final int maxLine;
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: kPrimaryColor,
+      maxLines: maxLine,
       decoration: InputDecoration(
-        hintText: 'title',
+        hintText: hint,
         // hintStyle: TextStyle(color: kPrimaryColor),
         border: buildBorder(),
         enabledBorder: buildBorder(),
